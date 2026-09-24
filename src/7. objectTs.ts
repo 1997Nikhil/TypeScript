612276822cb90568faf96a97eaccff12b9f8f751
Nikhil Dadhich <nikhildadhich58@gmail.com>
2026-09-24 12:37:23 +0530

    objectTs.ts

--- src/7. objectTs.ts
+++ src/7. objectTs.ts
@@ -78,10 +78,58 @@ type order = {
 }
 
 
-type Chai = {       // 
+type Chai = {        
     name : string,
     price : number,
     isHot : boolean
 }
 
-const updateChai = (updates : Partial)
+
+const updateChai = (updates : Partial<Chai>) => { 
+    console.log("updating chai with", updates)
+}
+
+
+updateChai({price: 25})
+updateChai({isHot: false})
+updateChai({})
+
+
+type ChaiOrder = {
+    name ?: string;
+    quantity ?: number
+}
+
+const placeOrder = (order : Required<ChaiOrder>) => {
+    console.log(order);
+}
+
+
+placeOrder({})
+
+
+
+type ChaiNew = {
+    name : string;
+    price : number;
+    isHot : boolean;
+    ingredients : string[];
+}
+
+
+type BasicChaiInfo = Pick<Chai, "name" | "price">
+
+const chaiInfo: BasicChaiInfo = {
+    name : "Lemon Tea",
+    price : 30
+}
+
+
+type chaiNew = {
+    name : string;
+    price : number;
+    isHot : boolean;
+    secretIngredients : string[]
+};
+
+type PublicChai = Omit<Chai, "secretIngredients">;
